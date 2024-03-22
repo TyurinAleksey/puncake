ab7e115731fb96717e10f1b3a8675bfffa779a84
mechtatel <alaxche@gmail.com>
2024-03-22 14:52:23 +1000

    para 22.03.2024 new

--- controlles/auth.js
+++ controlles/auth.js
@@ -1,11 +1,19 @@
+const User = require("../model/user")
+
+
 module.exports.login = function(req, res) {
     res.status(200).json({
-        login: true
+        login: {
+            email: req.body.email,
+            password: req.body.password
+        }
     })
 }
 
 module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: "from controlles"
+    const user = new User ({
+        email: req.body.email,
+        password: req.body.password,
     })
+    user.save().then(() => console.log("User created"))
 }
---
+++ db.js
@@ -0,0 +1,20 @@
+const {MongoClient} = require("mongodb");
+const URL = "mongodb://0.0.0.0:27017/cake"
+
+let dbConnection;
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient
+            .connect(URL)
+            .then((client) => {
+                console.log("Connected to MongoDB");
+                dbConnection = client.db();
+                return cb
+            })
+            .catch((err) => {
+                return cb (err)
+            })
+    },
+    getDb: () => dbConnection,
+}
